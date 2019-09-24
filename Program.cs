e8bf2ff0085b73ba5011df0147d1192792fafb2b
André <andre-nordquist@hotmail.com>
2019-09-24 09:05:18 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace TODO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
